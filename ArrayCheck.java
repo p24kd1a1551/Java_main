c91c97f58deac27a2e297555acda85fe0017c83f
Deepesh Abinay <deepeshabinay@gmail.com>
2026-05-06 08:12:42 +0530

    main

--- ArrayCheck.java
+++ ArrayCheck.java
@@ -3,20 +3,21 @@ import java.util.Scanner;
 
 public class ArrayCheck {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a[] = new int[5];
+        try (Scanner sc = new Scanner(System.in)) {
+            int a[] = new int[5];
 
-        System.out.println("Enter 5 elements:");
-        for(int i = 0; i < 5; i++) {
-            a[i] = sc.nextInt();
-        }
+            System.out.println("Enter 5 elements:");
+            for(int i = 0; i < 5; i++) {
+                a[i] = sc.nextInt();
+            }
 
-        System.out.println("Required elements are:");
-        for(int i = 0; i < 5; i++) {
-            if(i % 2 == 0 && a[i] % 2 == 0)
-                System.out.println(a[i]);
-            else if(i % 2 != 0 && a[i] % 2 != 0)
-                System.out.println(a[i]);
+            System.out.println("Required elements are:");
+            for(int i = 0; i < 5; i++) {
+                if(i % 2 == 0 && a[i] % 2 == 0)
+                    System.out.println(a[i]);
+                else if(i % 2 != 0 && a[i] % 2 != 0)
+                    System.out.println(a[i]);
+            }
         }
     }
 }
