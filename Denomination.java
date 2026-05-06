ec5b70d78fb3821c5e602cc5fc4424ea263c0547
Deepesh Abinay <deepeshabinay@gmail.com>
2026-05-06 08:11:42 +0530

    main

--- Denomination.java
+++ Denomination.java
@@ -3,23 +3,24 @@ import java.util.Scanner;
 
 public class Denomination {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int amount, total = 0;
+        try (Scanner sc = new Scanner(System.in)) {
+            int amount, total = 0;
 
-        System.out.println("Enter amount:");
-        amount = sc.nextInt();
+            System.out.println("Enter amount:");
+            amount = sc.nextInt();
 
-        int notes[] = {500, 200, 100, 50, 20, 10};
+            int notes[] = {500, 200, 100, 50, 20, 10};
 
-        for(int i = 0; i < notes.length; i++) {
-            int count = amount / notes[i];
-            if(count != 0) {
-                System.out.println(notes[i] + " notes = " + count);
-                total = total + count;
-                amount = amount % notes[i];
+            for(int i = 0; i < notes.length; i++) {
+                int count = amount / notes[i];
+                if(count != 0) {
+                    System.out.println(notes[i] + " notes = " + count);
+                    total = total + count;
+                    amount = amount % notes[i];
+                }
             }
-        }
 
-        System.out.println("Total notes = " + total);
+            System.out.println("Total notes = " + total);
+        }
     }
 }
